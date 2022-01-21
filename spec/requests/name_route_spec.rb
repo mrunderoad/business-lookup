9bce0f441149216b37865f4d4fd9ea7988aac146
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-01-21 10:59:40 -0800

    Name route specs passing

---
+++ spec/requests/name_route_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "search for aa business by name route", :type => :request do
+
+  let!(:businesses) { FactoryBot.create_list(:business, 20) }
+
+  before { get "/businesses?name=#{Business.first.name}" }
+
+  it 'returns one business' do
+    expect(JSON.parse(response.body).size).to eq(1)
+  end
+
+  it 'returns the business with the name: Silver Bakery' do
+    expect(JSON.parse(response.body).first['name']).to eq(Business.first.name)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
